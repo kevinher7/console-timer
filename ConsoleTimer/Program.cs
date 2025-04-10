969aa2f73e31ce0bc36c9f2beff622da69ccc8c3
Kevin Hernandez <kevinhernem@gmail.com>
2025-04-10 16:24:15 +0900

    feat: use Clock class in entry point

--- ConsoleTimer/Program.cs
+++ ConsoleTimer/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ConsoleTimer.Elements;
+
+namespace ConsoleTimer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Clock clock = new Clock();
+
+            double currentTime = clock.getCurrentTime();
+            Console.WriteLine($"Current Time: {currentTime}"); // Use string interpolation
+            Console.ReadKey();
+        }
+    }
+}
