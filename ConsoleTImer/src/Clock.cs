43077ab75cba19f5101fecce2fb8256887738a50
Kevin Hernandez <kevinhernem@gmail.com>
2025-04-10 15:39:12 +0900

    feat: initialize clock class

---
+++ ConsoleTImer/src/Clock.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleTimer.Elements
+{
+    public class Clock
+    {
+        private double _current_time = 0.0;
+
+        public double getCurrentTime()
+        {
+            return _current_time;
+        }
+
+    }
+}
